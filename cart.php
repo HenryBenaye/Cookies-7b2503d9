3dc10a23df03a0d2137adae8bc42026602e6d40a
HENRY BENAYE <2106747@talnet.nl>
2020-02-26 10:10:58 +0100

    Uitwerking van de exercise

---
+++ cart.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <h1>Skatboard (#1)</h1>
+    <h1>Basketbal (#2)</h2>
+    <h1>Skeelers (#3)</h3>
+    <form action="cart.php" method="get">
+        Keuze: <input type="text" name="keuze"> 
+        <input type="submit">
+    </form>
+    
+</body>
+</html>
+
+<?php   
+    if(isset($_GET['keuze'])){
+        setcookie("keuze", $_GET['keuze'], time()+ 20);
+
+    }
+
+
+    if (isset($_COOKIE['keuze'])) {
+        echo"Gekozen item:". $_COOKIE['keuze'];
+    }
+
+    
+   
+?>
